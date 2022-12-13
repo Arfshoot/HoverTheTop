e9108431178d432c412fe44f1093d35874114b8f
Jeremy <arfshoottv@gmail.com>
2022-12-13 14:05:08 +0100

    commit initialisation of Hoverthetop

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+
+import {Routes, Route} from 'react-router-dom'
+
+import Home from './pages/home/Home'
+import Contact from './pages/contact/contact'
+import Qui from './pages/qui/qui'
+import Realisation from './pages/realisation/realisation'
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Qui" element= {<Qui />} />
+        <Route path="/Réalisations" element={<Realisation />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="*" element={<Home />} />
+    </Routes>
+</div>
+    
   );
 }
 
---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './home.scss'
+
+
+import Logo from './../../images/Logo-home.png'
+
+export default function Home() {
+  return (
+    <div>
+        Demarre ton projet guedin
+    </div>
+  )
+}
